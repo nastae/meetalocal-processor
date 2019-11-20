e714d7c278ef18c6e6eaafccd5ca8a0e33515b2d
nastae <saauriscom>
2019-11-20 12:50:07 +0200

    add java project

---
+++ src/main/java/lt/govilnius/MeetalocalProcessorApplication.java
@@ -0,0 +1,13 @@
+package lt.govilnius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetalocalProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetalocalProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/govilnius/MeetalocalProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.govilnius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetalocalProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
